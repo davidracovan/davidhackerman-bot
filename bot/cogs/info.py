54b892c552191b38114425fa87992ed3a77524e2
davidracovan <74030080+davidracovan@users.noreply.github.com>
2021-02-07 19:53:13 -0500

    fix

--- bot/cogs/info.py
+++ bot/cogs/info.py
@@ -17,9 +17,8 @@ class Info(commands.Cog):
     async def about(self, ctx):
         """View information about the bot."""
         embed = tools.create_embed(ctx, 'About')
-        author1 = await ctx.guild.fetch_member(688530998920871969)
-        author2 = await ctx.guild.fetch_member(654874992672112650)
-        embed.add_field(name='Authors', value=f'{author1.mention} and {author2.mention}', inline=False)
+        author = await ctx.guild.fetch_member(688530998920871969)
+        embed.add_field(name='Authors', value=f'{author.mention}', inline=False)
         embed.add_field(name='Language', value='Python', inline=False)
         embed.add_field(name='Version', value='1.0', inline=False)
         await ctx.send(embed=embed)
